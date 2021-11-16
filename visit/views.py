7d3951babd48a226818d9bf41128cce2799aa5c1
Asus <azimimohsen06@gmail.com>
2021-11-16 14:25:41 +0330

    import jdatetime

--- visit/views.py
+++ visit/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from datetime import datetime
+from jdatetime import datetime
 
 from visit.models import *
 from visit.forms import *
